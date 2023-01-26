bbefacefc39fa241f39efe1da9472771a0433d93
GabJen <gabjenramos@gmail.com>
2023-01-26 13:30:02 -0300

    Add script to remove/add .responsive-menu class name

--- menu-script.js
+++ menu-script.js
@@ -2,6 +2,27 @@ const menuIcon = document.querySelector('.menu');
 const responsiveMenu = document.querySelector('img + div');
 const closeMenu = document.querySelector('.close-menu');
 
+//ADD/REMOVE RESPONSIVE-MENU CLASS NAME
+
+let windowWidth = window.innerWidth;
+
+function toggleClass() {
+    if(windowWidth < 1000) {
+        responsiveMenu.classList = 'responsive-menu';
+    } else {
+        responsiveMenu.removeAttribute('class');
+    }
+}
+
+toggleClass();
+
+window.addEventListener('resize', () => {
+    windowWidth = window.innerWidth;
+    toggleClass()
+});
+
+// SCRIPT TO OPEN/CLOSE MOBILE-MENU
+
 menuIcon.addEventListener('click', () => {
     responsiveMenu.style.display = 'flex';
     document.body.style.overflow = 'hidden';
@@ -10,4 +31,8 @@ menuIcon.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     responsiveMenu.style.display = 'none';
     document.body.style.overflow = 'auto';
-})
+});
+
+
+
+
