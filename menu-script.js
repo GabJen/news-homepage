b3f2a1fcac9e3781deaa740075d95e65ec3c93ca
GabJen <gabjenramos@gmail.com>
2023-12-25 19:25:47 -0300

    Fix menu responsiveness issues

--- menu-script.js
+++ menu-script.js
@@ -1,38 +1,9 @@
-const menuIcon = document.querySelector('.menu');
-const responsiveMenu = document.querySelector('img + div');
-const closeMenu = document.querySelector('.close-menu');
+const menuIcons = document.querySelectorAll('.menu-icons');
+const menu = document.querySelector('#menu');
 
-//ADD/REMOVE RESPONSIVE-MENU CLASS NAME
-
-let windowWidth = window.innerWidth;
-
-function toggleClass() {
-    if(windowWidth < 1000) {
-        responsiveMenu.classList = 'responsive-menu';
-    } else {
-        responsiveMenu.removeAttribute('class');
-    }
-}
-
-toggleClass();
-
-window.addEventListener('resize', () => {
-    windowWidth = window.innerWidth;
-    toggleClass()
+menuIcons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+        menu.classList.toggle('responsive-menu')
+    })
 });
-
-// SCRIPT TO OPEN/CLOSE MOBILE-MENU
-
-menuIcon.addEventListener('click', () => {
-    responsiveMenu.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-});
-
-closeMenu.addEventListener('click', () => {
-    responsiveMenu.style.display = 'none';
-    document.body.style.overflow = 'auto';
-});
-
-
-
 
