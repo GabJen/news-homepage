93f742c65aedbda405a2acfdf35539d4331c514c
GabJen <gabjenramos@gmail.com>
2023-12-26 19:15:11 -0300

    Comment code

--- menu-script.js
+++ menu-script.js
@@ -2,6 +2,7 @@ const menuIcons = document.querySelectorAll('.menu-icons');
 const menu = document.querySelector('#menu');
 const navItems = document.querySelectorAll('.nav-item');
 
+// Open/close responsive menu
 function openCloseMenu(element) {
     element.forEach(e => {
         e.addEventListener('click', () => {
